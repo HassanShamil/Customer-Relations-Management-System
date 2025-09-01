10edc7f9907c401ff5418848e72998bdf3cdeb7d
HassanShamil <hassanshamil55@gmail.com>
2025-09-01 22:06:37 +0530

    implement user login and registration

---
+++ CRM System/app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+
+    public function register(Request $request) {
+        $incomingFields = $request->validate([
+            'name' => ['required','min:3', Rule::unique('users','name')],
+            'email' => ['required', 'email',Rule::unique('users','email')], 
+            'password' => ['required', 'min:4'],
+            'confirm_password' => ['required', 'same:password'],
+        ]);
+
+        logger('Validation passed:', $incomingFields);
+
+        unset($incomingFields['confirm_password']);  
+        $incomingFields['password'] = bcrypt($incomingFields['password']); // hashing password 
+        $user = User::create($incomingFields);
+
+        if (!$user) {
+            logger('User creation failed');
+            return 'User creation failed';
+    }
+
+        auth() -> login($user);
+        
+        return redirect('/');
+    }
+
+    public function login(Request $request) {
+        $incomingFields = $request-> validate([ 
+            'loginname' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        // if (auth()->attempt(['name' => $incomingFields['loginname'], 'password'=> $incomingFields['loginpassword']])) {
+        //     $request -> session()-> regenerate();
+        // }
+
+        
+
+        // return redirect('/');
+        if (auth()->attempt(['name' => $incomingFields['loginname'], 'password'=> $incomingFields['loginpassword']])) {
+            $request->session()->regenerate();
+            return redirect('/');
+        }
+
+        return back()->withErrors(['loginname' => 'Invalid credentials']);
+    }
+
+    public function logout() {
+        auth()-> logout();
+        return redirect('/');
+    }
+}
--- CRM System/resources/views/home.blade.php
+++ CRM System/resources/views/home.blade.php
@@ -5,6 +5,56 @@
     <title>home</title>
 </head>
 <body>
-    <h1>Welcome !!!</h1>
+
+    @auth
+        
+    <p>Welcome {{ auth()->user()->name }} !</p>
+    <form action="/logout" method="POST">
+        @csrf
+        <button type="submit">Log out</button>
+    </form>
+
+
+
+
+    @else
+
+    <div style="border: 3px solid black; margin-bottom: 2px">
+        <h1>Log in</h1>
+        <form action="/login" method="POST">
+            @csrf     
+            <label for="loginname">username</label>
+            <input name="loginname" type="text" id="loginname">
+            @if ($errors->has('loginname'))
+                <div style="color: red;">{{ $errors->first('loginname') }}</div>
+            @endif
+            <label for="loginpassword">password</label>
+            <input name="loginpassword" type="password" id="loginpassword">
+            <button type="submit">Log in</button>
+        </form>
+    </div>
+
+
+    <div style="border: 3px solid black">
+        <h1>Sign up</h1>
+        <form action="/register" method="POST">
+            @csrf
+            <label for="name">username</label>
+            <input name="name" type="text" id="name">
+            <label for="email">email</label>
+            <input name="email" type="text" id="email">
+            <label for="password">password</label>
+            <input name="password" type="password" id="password">
+            <label for="confirm_password">confirm password</label>
+            <input name="confirm_password" type="password" id="confirm_password">
+            <button type="submit">Register</button>
+        </form>
+    </div>
+
+
+    @endauth
+
+
+
 </body>
 </html>
